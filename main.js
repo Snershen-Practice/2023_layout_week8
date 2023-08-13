456076876f3856c7c06c7523c8229d886008a74f
Wu Jane <jane72418@gmail.com>
2023-08-13 23:16:42 +0800

    fixed: rwd & swiper

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { gsap } from "gsap";
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";
 
 AOS.init();
-// console.log("Hello world!");
 
 const introSwiper = new Swiper(".intro-swiper", {
   pagination: {
     el: ".swiper-pagination",
-    // type: "progressbar",
   },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -17,28 +16,46 @@ const introSwiper = new Swiper(".intro-swiper", {
 });
 
 const commentSwiper = new Swiper(".comment-swiper", {
-  slidesPerView: "auto",
+  slidesPerView: "1",
+  // initialSlide: 0,
+
+  observer: true,
+  observeParents: true,
+  enabled: false,
   scrollbar: {
     el: ".swiper-scrollbar",
-    // hide: true,
+    dragSize: "auto",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: "2",
+      enabled: true,
+      // spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: "auto",
+      enabled: true,
+      // spaceBetween: 40,
+    },
   },
 });
 
 const productSwiper = new Swiper(".product-swiper", {
   slidesPerView: "1",
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    // hide: true,
-  },
+  initialSlide: 0,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  // observer: true,
+  // observeParents: true,
 });
+
+// productSwiper.init();
 
 const triggerTabList = document.querySelectorAll("#myTab button");
 triggerTabList.forEach((triggerEl) => {
@@ -48,4 +65,13 @@ triggerTabList.forEach((triggerEl) => {
     event.preventDefault();
     tabTrigger.show();
   });
+});
+
+gsap.to(".banner__card", {
+  y: -200,
+  duration: 0.25,
+  stagger: {
+    each: 0.1,
+    from: "center",
+  },
 });
